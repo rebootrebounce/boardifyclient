2011372245c288079a39a12f123fb89e1fda5b93
rockhardcode <rakeshrulz99@gmail.com>
2024-12-27 07:57:27 +0530

    changes

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -9,8 +9,7 @@ const axiosInstance = axios.create({
 
 // Add a request interceptor to include the token in headers
 axiosInstance.interceptors.request.use((config) => {
-  const token = Cookies.get('token');
-  console.log(token, "token")
+  const token = Cookies.get('tokens');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
